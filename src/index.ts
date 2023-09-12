e846030204180302de03f08030b88db1840751f1
marcela-celani <marcelacelani.ti@gmail.com>
2023-09-12 11:56:19 -0300

    porta 3003 configurada

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import { users, product } from "./database"
+// import { users, product } from "./database"
+import express from 'express'
+import cors from 'cors'
 
+// console.table(users)
+// console.table(product)
 
-console.table(users)
-console.table(product)
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log('Servidor rodando na porta 3003')
+})
